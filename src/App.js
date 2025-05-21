2bf3920e11a5ad15f831bc087d12f616cc0bcac7
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-21 14:09:52 +1000

    7.1 to do list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 //* To Do list
 
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+    console.log(toDo);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write a To Do..."
+        />
+        <button>Add a To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
