9bffc139330ec0b22dd5904f0b16d2d62c518dac
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-21 16:41:14 +1000

    coin trackercorsheaders.middleware.CorsMiddleware,     django.middleware.security.SecurityMiddleware,

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
-//* To Do list
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [coin, setCoin] = useState({});
+  const [usd, setUSD] = useState(0);
   const onChange = (event) => {
-    setToDo(event.target.value);
-    console.log(toDo);
+    setUSD(event.target.value);
+    console.log(usd);
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // console.log(toDo);
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const onCoinSelect = (event) => {
+    // if (event.target.value === "") {
+    //   setCoin({});
+    //   return;
+    // }
+    const selectedCoin = coins.find((coin) => coin.id === event.target.value);
+    setCoin(selectedCoin);
   };
-  console.log(toDos);
+  console.log(coin);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        //! const defaultCoin = coins.find((coin) => coin.id === "btc-bitcoin");
+        //! => coins is not defined yet! => use json!
+        const defaultCoin = json.find((coin) => coin.id === "btc-bitcoin");
+        setCoin(defaultCoin);
+        setLoading(false);
+      });
+  }, []);
+  // [x]: create an input, if 20 usd, show how much bitcoin can be bought
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write a To Do..."
-        />
-        <button>Add a To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coin ₿ ({loading ? "" : coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <input
+            value={usd}
+            onChange={onChange}
+            type="number"
+            placeholder="Write amount of USD..."
+          />
+          {/* <select onChange={onCoinSelect} value={coin?.id || ""}> */}
+          <select onChange={onCoinSelect} value={coin?.id || "btc-bitcoin"}>
+            {/* <option value="">Select Option</option> */}
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          {/* {!coin || Object.keys(coin).length === 0 ? null : ( */}
+          {Object.keys(coin).length === 0 ? null : (
+            <span>
+              You can buy {usd / coin.quotes.USD.price} of {coin.name}
+            </span>
+          )}
+        </div>
+      )}
     </div>
   );
 }
