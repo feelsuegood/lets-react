82814db0a894bc366da018f4a389c651ef630325
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-22 20:05:03 +1000

    router basename, styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
+  // Get basename from package.json homepage or default to "/"
+  const basename = process.env.PUBLIC_URL;
+
   return (
-    <Router>
+    <Router basename={basename}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -33,11 +34,13 @@ function Home() {
     getMovies();
   }, []);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               //! key is so importantttt
@@ -45,6 +48,7 @@ function Home() {
               id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
+              year={movie.year}
               summary={movie.summary}
               genres={movie.genres}
             />
